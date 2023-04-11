c3c5f2838c16973c5d6beb85694f196fb21c25de
Moritz Mähr <moritz.maehr@gmail.com>
2023-04-11 18:17:50 +0200

    wip

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,15 +2,26 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	daisyui: {
-		themes: ['cmyk']
+		themes: [
+			{
+				'stadt-geschichte-basel': {
+					primary: '#004488',
+					secondary: '#EECC66',
+					accent: '#EE99AA',
+					neutral: '#000000',
+					'base-100': '#F0F0F0',
+					info: '#6699CC',
+					success: '#EE99AA',
+					warning: '#997700',
+					error: '#994455'
+				}
+			}
+		]
 	},
 	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms'), require('daisyui')],
 	theme: {
 		fontFamily: {
 			sans: ['Euclid Circular B', 'sans-serif']
 		}
-		// container: {
-		// 	padding: '2rem'
-		// }
 	}
 };
