cdfca474d49f0fb6e5505b8b524638d1b53d1f6d
Moritz Mähr <moritz.maehr@gmail.com>
2023-05-24 15:07:15 +0200

    wip

--- src/routes/(pages)/blog/[slug]/+layout@.svelte
+++ src/routes/(pages)/blog/[slug]/+layout@.svelte
@@ -9,7 +9,7 @@
 		<a href={$page.url.pathname}>{$page.data.title.rendered}</a>
 	</li>
 </ol>
-<!-- <article class="container prose mx-auto p-2 xl:prose-xl lg:p-4"> -->
+
 <article class="container space-y-4 p-10">
 	<slot />
 </article>
