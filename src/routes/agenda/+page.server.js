4ec1d0ac581e4039dabbd45b7ed8d0f18edceed8
Moritz Mähr <moritz.maehr@gmail.com>
2023-06-30 11:44:35 +0200

    wip

--- src/routes/agenda/+page.server.js
+++ src/routes/agenda/+page.server.js
@@ -8,12 +8,13 @@ export async function load() {
 	const data = await parseStringPromise(xml);
 	const activities = data['kdz:exportActivities']['Activities'][0]['Activity'];
 	const parsedActivities = activities.map((activity) => {
-		const dates = activity.ActivityDates?.[0]?.ActivityDate?.map((date) => ({
-			startDate: date.$.startDate,
-			endDate: date.$.endDate,
-			startTime: date.$.startTime,
-			endTime: date.$.endTime,
-		})) ?? [];
+		const dates =
+			activity.ActivityDates?.[0]?.ActivityDate?.map((date) => ({
+				startDate: date.$.startDate,
+				endDate: date.$.endDate,
+				startTime: date.$.startTime,
+				endTime: date.$.endTime
+			})) ?? [];
 
 		return {
 			owner: activity['$'].owner,
@@ -21,18 +22,22 @@ export async function load() {
 			title: activity.Title[0],
 			shortDescription: activity.ShortDescription[0],
 			originUrl: activity.OriginURL[0],
-			dates,
+			dates
 		};
 	});
 	const allOwners = parsedActivities.map((item) => item.owner);
 	const uniqueOwners = [...new Set(allOwners)];
 	// console.log(uniqueOwners);
 	const filteredActivities = parsedActivities.filter((item) => {
-		return item.owner === 'Kunstmuseum Basel' || item.owner === 'Museum der Kulturen Basel' || item.owner === 'Antikenmuseum Basel und Sammlung Ludwig';
+		return (
+			item.owner === 'Kunstmuseum Basel' ||
+			item.owner === 'Museum der Kulturen Basel' ||
+			item.owner === 'Antikenmuseum Basel und Sammlung Ludwig'
+		);
 	});
 	// console.log(filteredActivities);
 	// TODO make one entry per date and time
 	return {
-		filteredActivities,
+		filteredActivities
 	};
 }
