e8b5d7afdc722db07c0b1f57665a4a34dca693cf
Moritz Mähr <moritz.maehr@gmail.com>
2023-07-07 00:09:16 +0200

    wip

---
+++ src/lib/Head.svelte
@@ -0,0 +1,40 @@
+<svelte:head>
+	<title>Stadt.Geschichte.Basel</title>
+	<meta name="keywords" content="Basel, Geschichte, Stadtgeschichte, Kultur" />
+	<meta
+		name="description"
+		content="Entdecken Sie die faszinierende Geschichte Basels. Tauchen Sie ein in die Kultur und Geschichte der Stadt am Rheinknie."
+	/>
+	<meta name="author" content="Stiftung Stadt.Geschichte.Basel" />
+
+	<!-- Twitter tags -->
+	<meta name="twitter:title" content="Stadt.Geschichte.Basel" />
+	<meta
+		name="twitter:description"
+		content="Entdecken Sie die faszinierende Geschichte Basels. Tauchen Sie ein in die Kultur und Geschichte der Stadt am Rheinknie."
+	/>
+	<meta name="twitter:site" content="@basel_stadt" />
+	<meta name="twitter:creator" content="@basel_stadt" />
+	<meta
+		property="twitter:image"
+		content="https://beta.stadtgeschichtebasel.ch/android-chrome-512x512.png"
+	/>
+	<meta property="twitter:image:width" content="512" />
+	<meta property="twitter:image:height" content="512" />
+
+	<!-- Open Graph / Facebook tags-->
+	<meta name="og:title" content="Stadt.Geschichte.Basel" />
+	<meta
+		name="og:description"
+		content="Entdecken Sie die faszinierende Geschichte Basels. Tauchen Sie ein in die Kultur und Geschichte der Stadt am Rheinknie."
+	/>
+	<meta property="og:url" content="https://stadtgeschichtebasel.ch/" />
+	<meta property="og:site_name" content="Stadt.Geschichte.Basel" />
+	<meta property="og:type" content="website" />
+	<meta
+		property="og:image"
+		content="https://beta.stadtgeschichtebasel.ch/android-chrome-512x512.png"
+	/>
+	<meta property="og:image:width" content="512" />
+	<meta property="og:image:height" content="512" />
+</svelte:head>
