b5712bcac928f5617746eb8d657229a84020d9ad
Moritz Mähr <moritz.maehr@gmail.com>
2023-07-14 15:21:39 +0200

    wip

--- src/routes/credits.json/+server.js
+++ src/routes/credits.json/+server.js
@@ -1,7 +1,6 @@
 import * as config from '$lib/config';
 import { json } from '@sveltejs/kit';
 
-// This can be false if you're using a fallback (i.e. SPA mode)
 export const prerender = true;
 
 /** @type {import('./$types').RequestHandler} */
