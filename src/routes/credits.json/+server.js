a9ac6d39e346bb3d26f8c66e681e2c1151e24f4c
Moritz Mähr <moritz.maehr@gmail.com>
2023-08-08 00:51:41 +0200

    wip

--- src/routes/credits.json/+server.js
+++ src/routes/credits.json/+server.js
@@ -45,11 +45,11 @@ async function getCredits() {
 			fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contributors`)
 				.then((res) => res.json())
 				.then((data) =>
-					data.map((contributor) => ({
+					data && Array.isArray(data) ? data.map((contributor) => ({
 						login: contributor.login,
 						avatar_url: contributor.avatar_url,
 						html_url: contributor.html_url
-					}))
+					})) : []
 				),
 			fetch(`${apiUrl}?per_page=1&status=success`)
 				.then((res) => res.json())
