a9eed0d1dcfe1ede058b15f9d16ccd861598d66e
Moritz Mähr <moritz.maehr@gmail.com>
2023-08-10 11:38:29 +0200

    pagination added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,42 @@
-<script lang="ts">
+<script>
 	import * as config from '$lib/config';
+	import { Paginator } from '@skeletonlabs/skeleton';
 
 	export let data;
+
+	// PaginatorSettings
+	let page = {
+		offset: 0,
+		limit: 10,
+		size: data.posts.length,
+		amounts: [10, 100]
+	};
+
+	$: paginatedPosts = data.posts.slice(
+		page.offset * page.limit, // start
+		page.offset * page.limit + page.limit // end
+	);
 </script>
 
 <svelte:head>
 	<title>{config.title}</title>
 </svelte:head>
+
+<div class="container space-y-4 p-10">
+	<h1>Blog</h1>
+
+	{#each paginatedPosts as post}
+		<article>
+			<h2>
+				<a href={`/blog/${post.slug}`}>{@html post.title}</a>
+			</h2>
+			<p class="date">{post.date}</p>
+			<p class="description">{@html post.excerpt}</p>
+		</article>
+	{/each}
+
+	<Paginator bind:settings={page} showNumerals />
+</div>
 
 <!-- Posts -->
 <!-- <section>
@@ -20,18 +50,3 @@
 		{/each}
 	</ul>
 </section> -->
-
-
-<div class="container space-y-4 p-10">
-	<h1>Blog</h1>
-
-	{#each data.posts as post}
-		<article>
-			<h2>
-				<a href={`/blog/${post.slug}`}>{@html post.title}</a>
-			</h2>
-			<p class="date">{post.date}</p>
-			<p class="description">{@html post.excerpt}</p>
-		</article>
-	{/each}
-</div>
