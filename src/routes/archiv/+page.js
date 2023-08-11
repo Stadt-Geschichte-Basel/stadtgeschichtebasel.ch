65f85d849fe82f993273286047a631c615087fca
Moritz Mähr <moritz.maehr@gmail.com>
2023-08-11 19:11:07 +0200

    wip

--- src/routes/archiv/+page.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('./$types').PageLoad}*/
-export async function load({ fetch, params }) {
-	const res = await fetch(`/archive.json`);
-	const urls = await res.json();
-	return {
-		urls
-	};
-}
