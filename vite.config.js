23443b766655d23d05d9e584db26ac6f09ca3f5a
Moritz Mähr <moritz.maehr@gmail.com>
2023-09-01 11:47:03 +0200

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,44 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vitest/config';
+import { imagetools } from 'vite-imagetools';
+
+const supportedExtensions = ['png', 'jpg', 'jpeg'];
 
 export default defineConfig({
 	plugins: [
+		// imagetools({
+		// 	defaultDirectives: (url) => {
+		// 		const extension = url.pathname.substring(
+		// 			url.pathname.lastIndexOf('.') + 1
+		// 		);
+		// 		if (supportedExtensions.includes(extension)) {
+		// 			return new URLSearchParams({
+		// 				width: '640;768;1024;1280;1536',
+		// 				format: 'avif;webp;' + extension,
+		// 				// picture: true
+		// 				srcset: true
+		// 			});
+		// 		}
+		// 		return new URLSearchParams();
+		// 	}
+		// 	// defaultDirectives: (url) => {
+		// 	// 	return new URLSearchParams(
+		// 	// 		'w=640;768&format=jpg'
+		// 	// 		// 'w=640;768;1024;1280;1536&format=avif;webp;jpg&as=picture'
+		// 	// 	)
+		// 	// }
+		// }),
 		sveltekit(),
-
 		SvelteKitPWA({
 			registerType: 'autoUpdate',
 			manifest: false,
 			manifestFile: 'manifest.webmanifest',
 			scope: '/',
-			workbox: {
-				globPatterns: ['pages.json', 'posts.json', '**/*.{js,css,html,svg,ico,png,webp,avif}'],
-				globIgnores: ['**/sw*', '**/workbox-*']
-			}
+			// workbox: {
+			// 	globPatterns: ['pages.json', 'posts.json', '**/*.{js,css,html,svg,ico,jpg,png,webp,avif}'],
+			// 	globIgnores: ['**/sw*', '**/workbox-*']
+			// }
 		})
 	],
 
