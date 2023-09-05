5397d932161f81467e91bcbb3f03c8102c8df523
Moritz Mähr <moritz.maehr@gmail.com>
2023-09-05 16:34:29 +0200

    wip

--- src/lib/components/Breadcrumbs.svelte
+++ src/lib/components/Breadcrumbs.svelte
@@ -5,7 +5,7 @@
 <nav class="text-token flex justify-center p-4" aria-label="Breadcrumbs">
 	<ol class="breadcrumb">
 		<li class="crumb"><a class="anchor" href="/">Blog</a></li>
-		<li class="crumb-separator" aria-hidden>/</li>
+		<li class="crumb-separator" aria-hidden="true">/</li>
 		<li>
 			<a class="anchor" href={$page.url.pathname} aria-current="page">{$page.data.meta.title}</a>
 		</li>
