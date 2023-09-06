4f24fca49f1b8070c39e0942834a7411cd752289
Moritz Mähr <moritz.maehr@gmail.com>
2023-09-06 20:45:14 +0200

    wip

--- src/routes/index/[...path]/+page.svelte
+++ src/routes/index/[...path]/+page.svelte
@@ -15,14 +15,14 @@
 	<h1>Wir sind umgezogen</h1>
 	<p>
 		Eine archivierte Version dieser Seite finden Sie <a
-			href="https://web.archive.org/web/*/https://www.stadtgeschichtebasel.ch/index/{data.url}"
+			href="https://web.archive.org/web/*/https://www.stadtgeschichtebasel.ch/index/{data.url}.html"
 			>hier</a
 		>.
 	</p>
 </Container>
 
 <iframe
-	src="https://web.archive.org/web/*/https://www.stadtgeschichtebasel.ch/index/{data.url}"
+	src="https://web.archive.org/web/*/https://www.stadtgeschichtebasel.ch/index/{data.url}.html"
 	class="w-full h-96"
 	frameborder="0"
 	title="Archive.org"
