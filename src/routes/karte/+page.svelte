ae45e9396ffeb649b9036011626be1dfbba08592
Moritz Mähr <moritz.maehr@gmail.com>
2023-09-07 11:02:05 +0200

    wip

--- src/routes/karte/+page.svelte
+++ src/routes/karte/+page.svelte
@@ -19,67 +19,79 @@
 		});
 	}
 
-	function handleLegendItemKeydown(event, feature) {
-		if (['Enter', ' '].includes(event.key)) {
-			flyToFeature(feature);
+	function handleSelectChange(event) {
+		const selectedFeature = featuresWithLabels.find((f) => f.label === event.target.value);
+		if (selectedFeature) {
+			flyToFeature(selectedFeature);
 		}
 	}
 </script>
 
-<section class="flex flex-wrap">
-		<MapLibre
-			style="https://vectortiles.geo.admin.ch/styles/ch.swisstopo.leichte-basiskarte.vt/style.json"
-		class="h-screen w-full overflow-hidden md:h-auto md:w-3/4"
-			zoom={14}
-			maxZoom={20}
-			center={[7.59274, 47.55094]}
-			maxBounds={[
-				[5.94, 45.81],
-				[10.51, 47.81]
-			]}
-			bind:map
+<section class="h-[94vh] w-full overflow-hidden">
+	<!-- Dropdown for features -->
+	<div class="absolute z-10 p-4">
+		<select class="rounded bg-white p-2 shadow-md" on:change={handleSelectChange}>
+			<option value="">Springe zu</option>
+			{#each featuresWithLabels as feature}
+				<option value={feature.label}>{feature.label}</option>
+			{/each}
+		</select>
+	</div>
+
+	<!-- Full width and height map -->
+	<MapLibre
+		style="https://vectortiles.geo.admin.ch/styles/ch.swisstopo.leichte-basiskarte.vt/style.json"
+		class="h-full w-full"
+		zoom={14}
+		maxZoom={20}
+		center={[7.59274, 47.55094]}
+		maxBounds={[
+			[5.94, 45.81],
+			[10.51, 47.81]
+		]}
+		bind:map
+	>
+		<GeoJSON
+			id="data"
+			{data}
+			cluster={{
+				radius: 1000,
+				maxZoom: 15,
+				properties: {
+					// Sum the `mag` property from all the points in each cluster.
+					total_mag: ['+', ['get', 'geometry.coordinates[0]']]
+				}
+			}}
 		>
-			<GeoJSON
-				id="data"
-				{data}
-				cluster={{
-					radius: 1000,
-					maxZoom: 15,
-					properties: {
-						// Sum the `mag` property from all the points in each cluster.
-						total_mag: ['+', ['get', 'geometry.coordinates[0]']]
-					}
+			<CircleLayer
+				applyToClusters
+				id="clusters"
+				hoverCursor="pointer"
+				paint={{
+					// Use step expressions (https://maplibre.org/maplibre-gl-js-docs/style-spec/#expressions-step)
+					// with three steps to implement three types of circles:
+					//   * Blue, 20px circles when point count is less than 100
+					//   * Yellow, 30px circles when point count is between 100 and 750
+					//   * Pink, 40px circles when point count is greater than or equal to 750
+					'circle-color': ['step', ['get', 'point_count'], '#51bbd6', 3, '#f1f075', 5, '#f28cb1'],
+					'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40],
+					'circle-stroke-color': '#f00',
+					'circle-stroke-width': 1
 				}}
-			>
-				<CircleLayer
-					applyToClusters
-					id="clusters"
-					hoverCursor="pointer"
-					paint={{
-						// Use step expressions (https://maplibre.org/maplibre-gl-js-docs/style-spec/#expressions-step)
-						// with three steps to implement three types of circles:
-						//   * Blue, 20px circles when point count is less than 100
-						//   * Yellow, 30px circles when point count is between 100 and 750
-						//   * Pink, 40px circles when point count is greater than or equal to 750
-						'circle-color': ['step', ['get', 'point_count'], '#51bbd6', 3, '#f1f075', 5, '#f28cb1'],
-						'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40],
-						'circle-stroke-color': '#f00',
-						'circle-stroke-width': 1
-					}}
-					on:click={(e) => {
-						map.flyTo({
-							center: e.detail.features[0].geometry.coordinates,
-							zoom: map.getZoom() + 2,
-							speed: 0.5
-						});
-					}}
-					><!-- 
+				on:click={(e) => {
+					map.flyTo({
+						center: e.detail.features[0].geometry.coordinates,
+						zoom: map.getZoom() + 2,
+						speed: 0.5
+					});
+				}}
+				><!-- 
 					<Popup openOn="hover" closeOnClickInside let:features>
 						<ClusterPopup feature={features?.[0]} />
 					</Popup>-->
-				</CircleLayer>
+			</CircleLayer>
 
-				<!-- TODO: Funktioniert noch nicht. Soll die Anzahl Punkte pro Cluster anzeigen
+			<!-- TODO: Funktioniert noch nicht. Soll die Anzahl Punkte pro Cluster anzeigen
 				<SymbolLayer
 					applyToClusters
 					paint={{}}
@@ -104,23 +116,23 @@
 					}}
 				/>-->
 
-				<MarkerLayer applyToClusters={false} interactive let:feature>
-					<div
-						class="rounded-full bg-red-300 p-3 text-sm font-bold shadow-2xl focus:outline-2 focus:outline-black"
-					>
-						{feature.properties.label}
-					</div>
+			<MarkerLayer applyToClusters={false} interactive let:feature>
+				<div
+					class="rounded-full bg-red-300 p-3 text-sm font-bold shadow-2xl focus:outline-2 focus:outline-black"
+				>
+					{feature.properties.label}
+				</div>
 
-					<Popup openOn="click" offset={[0, -10]} maxWidth={'30%'}>
-						<h3 class="text-lg font-bold">{feature.properties.name}</h3>
-						<p class="text-sm">{feature.properties.address}</p>
-						<p class="text-sm">
-							<a href={feature.properties.website} target="_blank">{feature.properties.website}</a>
-						</p></Popup
-					>
-				</MarkerLayer>
-			</GeoJSON>
-			<!--
+				<Popup openOn="click" offset={[0, -10]} maxWidth={'30%'}>
+					<h3 class="text-lg font-bold">{feature.properties.name}</h3>
+					<p class="text-sm">{feature.properties.address}</p>
+					<p class="text-sm">
+						<a href={feature.properties.website} target="_blank">{feature.properties.website}</a>
+					</p></Popup
+				>
+			</MarkerLayer>
+		</GeoJSON>
+		<!--
 			<Control class="flex flex-col gap-y-2">
 				<ControlGroup>
 					{#each featuresWithLabels as feature}
@@ -142,19 +154,5 @@
 				</ControlGroup>
 			</Control>
 		-->
-		</MapLibre>
-	
-	<div class="w-full overflow-y-scroll p-4 md:w-1/4">
-				{#each featuresWithLabels as feature}
-					<div
-				class="mb-1 cursor-pointer rounded bg-gray-300 p-1 hover:bg-gray-400"
-						on:click={() => flyToFeature(feature, 17)}
-						on:keydown={(event) => handleLegendItemKeydown(event, feature)}
-						tabindex="0"
-						role="button"
-					>
-						{feature.label}
-					</div>
-				{/each}
-	</div>
+	</MapLibre>
 </section>
