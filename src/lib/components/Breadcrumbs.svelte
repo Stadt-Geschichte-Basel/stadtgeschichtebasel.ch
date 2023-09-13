cdb565cdde94d0bb63c32107278e587b2dcc7aad
Moritz Mähr <moritz.maehr@gmail.com>
2023-09-13 14:29:10 +0200

    wip

--- src/lib/components/Breadcrumbs.svelte
+++ src/lib/components/Breadcrumbs.svelte
@@ -1,10 +1,11 @@
 <script>
 	import { page } from '$app/stores';
+	import { base } from '$app/paths';
 </script>
 
 <nav class="text-token flex justify-center p-4 " aria-label="Breadcrumbs">
 	<ol class="breadcrumb">
-		<li class="crumb"><a class="anchor" href="/">Blog</a></li>
+		<li class="crumb"><a class="anchor" href="{base}/">Blog</a></li>
 		<li class="crumb-separator" aria-hidden="true">/</li>
 		<li>
 			<a class="anchor" href={$page.url.pathname} aria-current="page" title={$page.data.meta.title}>Aktueller Eintrag</a>
--- src/routes/agenda/+page.svelte
+++ src/routes/agenda/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { base } from '$app/paths';
 	import Container from '$lib/components/Container.svelte';
 	/** @type {import('./$types').PageData} */
 	export let data;
@@ -33,15 +34,14 @@
 			<article class="card">
 				<h3 class="card-header">{event.owner}: {event.title}</h3>
 
-					<p class="card-footer">
-						<time datetime={event.startDate}>{event.startDate}</time>
-						{#if event.startTime}
-							(<time>{event.startTime}</time>
-							{#if event.endTime}- <time>{event.endTime}</time>{/if})
-						{/if}
-						{event.shortDescription} <a href={event.originUrl}>Mehr Infos</a>
-					</p>
-	
+				<p class="card-footer">
+					<time datetime={event.startDate}>{event.startDate}</time>
+					{#if event.startTime}
+						(<time>{event.startTime}</time>
+						{#if event.endTime}- <time>{event.endTime}</time>{/if})
+					{/if}
+					{event.shortDescription} <a href={event.originUrl}>Mehr Infos</a>
+				</p>
 			</article>
 		{/each}
 		<nav aria-label="Veranstaltungsnavigation">
@@ -52,8 +52,8 @@
 		<h2>Dauerausstellungen</h2>
 
 		{#each exhibitions as exhibition}
-		<article class="card">
-			<h3 class="card-header">{exhibition.owner}: {exhibition.title}</h3>
+			<article class="card">
+				<h3 class="card-header">{exhibition.owner}: {exhibition.title}</h3>
 				<p class="card-footer">
 					{#if exhibition.longDescription}
 						{exhibition.longDescription}
@@ -69,7 +69,7 @@
 		<h2>Agendabasel.ch</h2>
 		<p>
 			Alle Daten auf dieser Seite stammen von <a href="https://agendabasel.ch">agendabasel.ch</a>
-			und werden der Stiftung Stadt.Geschichte.Basel im Rahmen einer Kooperation kostenlos zur Verfügung
+			und werden der Stiftung <a href="{base}/ueber-uns">Stadt.Geschichte.Basel</a> im Rahmen einer Kooperation kostenlos zur Verfügung
 			gestellt. Falls Sie eine Veranstaltung oder Ausstellung auf Stadt.Geschichte.Basel publizieren
 			möchten, wenden Sie sich bitte direkt an agendabasel.ch oder an
 			<a href="mailto:vermittlung@stadtgeschichtebasel.ch">vermittlung@stadtgeschichtebasel.ch</a>.
