87913df375c7d83b070b341e1f92f34bf3f31324
Moritz Mähr <moritz.maehr@gmail.com>
2023-10-03 21:54:00 +0200

    fix: fix and refactoring navigation

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -4,71 +4,30 @@
 	import { getDrawerStore } from '@skeletonlabs/skeleton';
 
 	const drawerStore = getDrawerStore();
-	function drawerClose() {
-		drawerStore.close();
-	}
+	const drawerClose = () => drawerStore.close();
+	const navItems = [
+		{ label: 'Startseite', path: '/' },
+		{ label: 'Blog', path: '/blog' },
+		{ label: 'Meilensteine', path: '/meilensteine' },
+		{ label: 'Agenda', path: '/agenda' },
+		{ label: 'Orte', path: '/orte' },
+		{ label: 'Forschung', path: '/forschung' },
+		{ label: 'Über uns', path: '/ueber-uns' }
+	];
+
+	const isActive = (path) => $page.url.pathname === base + path || new String($page.url.pathname).startsWith(base + path);
 </script>
 
 <nav class="list-nav p-4 text-xl" aria-label="Hauptmenü" data-sveltekit-preload-data>
 	<ul>
-		<li aria-current={$page.url.pathname === base + '/' ? 'page' : undefined}>
-			<a
-				class="cursor-pointer {$page.url.pathname === base + '/' ? 'font-bold' : 'font-normal'}"
-				href="{base}/"
-				on:click={drawerClose}>Startseite</a
-			>
-		</li>
-		<li aria-current={$page.url.pathname === base + '/blog' ? 'page' : undefined}>
-			<a
-				class="cursor-pointer {$page.url.pathname === base + '/blog' ||
-				new String($page.url.pathname).startsWith(base + '/blog')
-					? 'font-bold'
-					: 'font-normal'}"
-				href="{base}/blog"
-				on:click={drawerClose}>Blog</a
-			>
-		</li>
-		<li aria-current={$page.url.pathname === base + '/meilensteine' ? 'page' : undefined}>
-			<a
-				class="cursor-pointer {$page.url.pathname === base + '/meilensteine'
-					? 'font-bold'
-					: 'font-normal'}"
-				href="{base}/meilensteine"
-				on:click={drawerClose}>Meilensteine</a
-			>
-		</li>
-		<li aria-current={$page.url.pathname === base + '/agenda' ? 'page' : undefined}>
-			<a
-				class="cursor-pointer {$page.url.pathname === base + '/agenda'
-					? 'font-bold'
-					: 'font-normal'}"
-				href="{base}/agenda"
-				on:click={drawerClose}>Agenda</a
-			>
-		</li>
-		<li aria-current={$page.url.pathname === base + '/orte' ? 'page' : undefined}>
-			<a
-				class="cursor-pointer {$page.url.pathname === base + '/orte'
-					? 'font-bold'
-					: 'font-normal'}"
-				href="{base}/orte"
-				on:click={drawerClose}>Orte</a
-			>
-		</li>
-		<li aria-current={$page.url.pathname === base + '/forschung' ? 'page' : undefined}>
-			<a
-				class="cursor-pointer {$page.url.pathname === base + '/forschung' ? 'font-bold' : 'font-normal'}"
-				href="{base}/forschung">Forschung</a
-			>
-		</li>
-		<li aria-current={$page.url.pathname === base + '/ueber-uns' ? 'page' : undefined}>
-			<a
-				class="cursor-pointer {$page.url.pathname === base + '/ueber-uns'
-					? 'font-bold'
-					: 'font-normal'}"
-				href="{base}/ueber-uns"
-				on:click={drawerClose}>Über uns</a
-			>
-		</li>
+		{#each navItems as { label, path }}
+			<li aria-current={isActive(path) ? 'page' : undefined}>
+				<a
+					class="cursor-pointer {isActive(path) ? 'font-bold' : 'font-normal'}"
+					href="{base}{path}"
+					on:click={drawerClose}>{label}</a
+				>
+			</li>
+		{/each}
 	</ul>
 </nav>
