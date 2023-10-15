73876e4548ef3b547ff6a1e331d0d240800df6c9
Moritz Mähr <moritz.maehr@gmail.com>
2023-10-15 19:51:34 +0200

    fix: aligned buttons

--- src/lib/components/PostNav.svelte
+++ src/lib/components/PostNav.svelte
@@ -3,10 +3,13 @@
     export let page
     export let lastPage
 </script>
-<nav aria-label="Blognavigation" class="mt-4 btn-group variant-filled">
+<nav aria-label="Blognavigation" class="mt-4 btn-group variant-filled flex justify-between">
     {#if page > 1}
         <a href="{base}/blog/{page - 1}" class="btn btn-xl" rel="prev" data-sveltekit-reload>Vorherige Seite</a>
     {/if}
+    <div>
+        <span class="btn disabled">Seite {page} von {lastPage}</span>
+    </div> <!-- Empty div to maintain space between -->
     {#if page < lastPage}
         <a href="{base}/blog/{page + 1}" class="btn btn-xl" rel="next" data-sveltekit-reload>Nächste Seite</a>
     {/if}
