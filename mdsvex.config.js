5fe31294dfa40fe9fca16133e7e49793b0498b2d
Moritz Mähr <moritz.maehr@gmail.com>
2024-01-09 17:07:49 +0100

    feat: remove unecessary preprocessing and update dependencies

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,10 +1,8 @@
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
-import { rehypeMdsvexImageAutoimport } from 'rehype-mdsvex-image-autoimport';
 import rehypeExternalLinks from 'rehype-external-links';
 import remarkCaptions from 'remark-captions';
-import remarkUnwrapImages from 'remark-unwrap-images';
 
 const config = defineConfig({
 	extensions: ['.svelte.md', '.md', '.svx'],
@@ -13,13 +11,8 @@ const config = defineConfig({
 		dashes: 'oldschool'
 	},
 
-	remarkPlugins: [remarkCaptions, remarkUnwrapImages],
-	rehypePlugins: [
-		rehypeExternalLinks, // Adds 'target' and 'rel' to external links
-		rehypeSlug, // Adds 'id' attributes to Headings (h1,h2,etc)
-		rehypeAutolinkHeadings,
-		rehypeMdsvexImageAutoimport
-	]
+	remarkPlugins: [remarkCaptions],
+	rehypePlugins: [rehypeSlug, [rehypeExternalLinks, { target: '_blank' }], rehypeAutolinkHeadings]
 });
 
 export default config;
