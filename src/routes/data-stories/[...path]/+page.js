dbdb6e127a5ab4283ddf1affdcf1544661bb2060
Moritz Mähr <maehr@users.noreply.github.com>
2024-01-30 19:08:00 +0100

    fix: link to make prerendereable

--- src/routes/data-stories/[...path]/+page.js
+++ src/routes/data-stories/[...path]/+page.js
@@ -1,10 +1,11 @@
 import { error } from '@sveltejs/kit';
+import data from '$lib/data/datastories.json';
 
 /** @type {import('./$types').PageLoad}*/
 export async function load({ params }) {
-	const datastories = ['https://storymaps.arcgis.com/stories/6be823309a7b4254aa27b5486703b68b'];
+	const foundEntry = data.find((entry) => entry.url === params.path);
 
-	if (datastories.includes(params.path)) {
+	if (foundEntry) {
 		return { url: params.path };
 	}
 
