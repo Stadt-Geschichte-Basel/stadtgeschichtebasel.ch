2efd9268f179121d9932fe0721c3d613069b8e05
Moritz Mähr <maehr@users.noreply.github.com>
2024-01-30 15:59:30 +0100

    fix: add correct base urls

--- src/routes/rss.xml/+server.js
+++ src/routes/rss.xml/+server.js
@@ -7,7 +7,7 @@ import * as config from '$lib/config';
 export const prerender = true;
 
 /** @type {import('./$types').RequestHandler} */
-export async function GET({ url, fetch }) {
+export async function GET({ fetch }) {
 	const posts = await fetch('/posts.json').then((res) => res.json());
 	const pages = await fetch('/pages.json').then((res) => res.json());
 	const staticPages = ['agenda', 'archiv', 'blog', 'credits', 'partner'];
@@ -16,15 +16,15 @@ export async function GET({ url, fetch }) {
 	rss += '<rss version="2.0">';
 	rss += '<channel>';
 	rss += `<title>${config.title}</title>`;
-	rss += `<link>${url.origin}</link>`;
+	rss += `<link>${config.url}</link>`;
 	rss += `<description>${config.description}</description>`;
 	rss += `<language>${config.lang}</language>`;
 
 	posts.forEach((post) => {
 		rss += `<item>`;
 		rss += `<title>${post.title}</title>`;
-		rss += `<link>${url.origin}/blog/${post.slug}</link>`;
-		rss += `<guid>${url.origin}/blog/${post.slug}</guid>`;
+		rss += `<link>${config.url}/blog/${post.slug}</link>`;
+		rss += `<guid>${config.url}/blog/${post.slug}</guid>`;
 		rss += `<pubDate>${new Date(post.date).toUTCString()}</pubDate>`;
 		rss += `<lastBuildDate>${new Date(post.modified).toUTCString()}</lastBuildDate>`;
 		rss += `<description>${post.excerpt}</description>`;
@@ -34,8 +34,8 @@ export async function GET({ url, fetch }) {
 	pages.forEach((page) => {
 		rss += `<item>`;
 		rss += `<title>${page.title}</title>`;
-		rss += `<link>${url.origin}/${page.slug}</link>`;
-		rss += `<guid>${url.origin}/${page.slug}</guid>`;
+		rss += `<link>${config.url}/${page.slug}</link>`;
+		rss += `<guid>${config.url}/${page.slug}</guid>`;
 		rss += `<pubDate>${new Date(page.date).toUTCString()}</pubDate>`;
 		rss += `<lastBuildDate>${new Date(page.modified).toUTCString()}</lastBuildDate>`;
 		rss += `<description>${page.excerpt}</description>`;
@@ -45,8 +45,8 @@ export async function GET({ url, fetch }) {
 	staticPages.forEach((page) => {
 		rss += `<item>`;
 		rss += `<title>${page}</title>`;
-		rss += `<link>${url.origin}/${page}</link>`;
-		rss += `<guid>${url.origin}/${page}</guid>`;
+		rss += `<link>${config.url}/${page}</link>`;
+		rss += `<guid>${config.url}/${page}</guid>`;
 		rss += `<pubDate>${new Date().toUTCString()}</pubDate>`;
 		rss += `<lastBuildDate>${new Date().toUTCString()}</lastBuildDate>`;
 		rss += `<description>${page}</description>`;
