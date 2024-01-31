6eadc2d306337748067ac3bfc7f65a5df173f633
Moritz Mähr <maehr@users.noreply.github.com>
2024-01-31 15:52:53 +0100

    feat: add jüdisches Milieu

--- src/routes/data-stories/[...path]/+page.js
+++ src/routes/data-stories/[...path]/+page.js
@@ -3,10 +3,16 @@ import data from '$lib/data/datastories.json';
 
 /** @type {import('./$types').PageLoad}*/
 export async function load({ params }) {
-	const foundEntry = data.find((entry) => entry.url === params.path);
+	const foundUrl = data.find((entry) => entry.url === params.path);
 
-	if (foundEntry) {
-		return { url: params.path };
+	if (foundUrl) {
+		return { url: foundUrl.url };
+	}
+
+	const foundSlug = data.find((entry) => entry.slug === params.path);
+
+	if (foundSlug) {
+		return { url: foundSlug.url };
 	}
 
 	error(404, `Could not find ${params.path}`);
