37a25028c6be6338ffa06a4286b79ad2a3f8794a
Moritz Mähr <maehr@users.noreply.github.com>
2024-02-10 16:25:38 +0100

    feat: add simple reporting mechanism to page

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -14,6 +14,14 @@
 		{ label: 'Projekt', path: '/projekt' },
 		{ label: 'Über uns', path: '/ueber-uns' }
 	];
+	const currentURL = $page.url.pathname;
+	const mailtoBody = encodeURI(
+		`Ich habe ein Problem auf der Seite entdeckt: ${currentURL}\n` +
+			`Art des Problems: [Bitte angeben: Technischer Fehler, Inhaltlicher Fehler, Tippfehler, Sonstiges]\n` +
+			`Beschreibung des Problems: [Bitte beschreiben Sie das Problem so genau wie möglich.]\n` +
+			`Weitere Anmerkungen: [Optional]\n` +
+			`Kontaktinformationen: [Optional, falls Rückmeldung erwünscht]\n`
+	);
 
 	$: isActive = (path) => {
 		if (path === '/blog' && $page.url.pathname.startsWith(base + path)) {
@@ -23,7 +31,11 @@
 	};
 </script>
 
-<nav class="list-nav p-4 text-xl" aria-label="Hauptmenü" data-sveltekit-preload-data>
+<nav
+	class="list-nav flex h-full flex-col justify-between p-4 text-xl"
+	aria-label="Hauptmenü"
+	data-sveltekit-preload-data
+>
 	<ul>
 		{#each navItems as { label, path }}
 			<li aria-current={isActive(path) ? 'page' : undefined}>
@@ -35,4 +47,10 @@
 			</li>
 		{/each}
 	</ul>
+
+	<a
+		class="variant-ghost btn btn-sm mt-4"
+		href="mailto:info@stadtgeschichtebasel.ch?subject=Fehler%20auf%20{currentURL}&body={mailtoBody}"
+		>Fehler melden</a
+	>
 </nav>
