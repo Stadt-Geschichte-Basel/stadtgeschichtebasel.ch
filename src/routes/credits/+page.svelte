8376a9eb3a3166945d40fa20040b7aa9b36bea73
Moritz Mähr <maehr@users.noreply.github.com>
2024-02-10 14:37:44 +0100

    feat: credits page updated

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -12,10 +12,29 @@
 <Container>
 	<h1>Credits</h1>
 
-	<h2>Mitwirkende</h2>
+	<h2>Gestaltung</h2>
 
 	<p>
-		Die folgenden Personen haben zum Quellcode von {config.title} beigetragen:
+		<a href="/">{config.domain}</a> wurde vom Team für Research Data Management und Public History entwickelt
+		und gestaltet. Namentlich von
+	</p>
+	<ul>
+		<li>Cristina Wildisen-Münch</li>
+		<li>Moritz Mähr</li>
+		<li>Moritz Twente</li>
+		<li>Nico Görlich</li>
+		<li>Noëlle Schnegg</li>
+	</ul>
+
+	<h2>Code</h2>
+
+	<p>
+		Die folgenden Personen haben zum <a
+			href="https://github.com/{config.githubHandle}/{config.githubRepo}/"
+			rel="”nofollow”"
+			target="_blank">Quellcode</a
+		>
+		von {config.title} beigetragen
 	</p>
 	<ul class="grid grid-cols-2 gap-4">
 		{#each data.contributors as contributor}
@@ -24,14 +43,16 @@
 					<img src={contributor.avatar_url} alt="Avatar of {contributor.login}" />
 				</div>
 				<div class="text-lg font-extrabold">
-					<a href={contributor.html_url}>{contributor.login}</a>
+					<a href={contributor.html_url} rel="”nofollow”" target="_blank">{contributor.login}</a>
 				</div>
 			</li>
 		{/each}
 	</ul>
 	<p>
-		Diese <a href="https://github.com/{config.githubHandle}/{config.githubRepo}/graphs/contributors"
-			>Liste</a
+		Diese <a
+			href="https://github.com/{config.githubHandle}/{config.githubRepo}/graphs/contributors"
+			rel="”nofollow”"
+			target="_blank">Liste</a
 		> wurde automatisch generiert. Co-authored Commits sind nicht berücksichtigt.
 	</p>
 
@@ -39,16 +60,33 @@
 
 	<p>
 		{config.title} wurde mit <a href="https://kit.svelte.dev/">SvelteKit</a>,
-		<a href="https://www.skeleton.dev/">Skeleton</a>
+		<a href="https://www.skeleton.dev/" rel="”nofollow”" target="_blank">Skeleton</a>
 		und vielen
-		<a href="https://github.com/{config.githubHandle}/{config.githubRepo}/blob/main/package.json"
-			>anderen Libraries</a
+		<a
+			href="https://github.com/{config.githubHandle}/{config.githubRepo}/blob/main/package.json"
+			rel="”nofollow”"
+			target="_blank">anderen Libraries</a
 		>
 		erstellt. Das Projekt ist Open Source (<a
 			href="https://github.com/{config.githubHandle}/{config.githubRepo}/blob/main/LICENSE.md"
-			>AGPL-3.0-Lizenz</a
+			rel="”nofollow”"
+			target="_blank">AGPL-3.0-Lizenz</a
 		>) und der Quellcode ist auf
-		<a href="https://github.com/Stadt-Geschichte-Basel/stadtgeschichtebasel.ch">GitHub</a>
+		<a
+			href="https://github.com/Stadt-Geschichte-Basel/stadtgeschichtebasel.ch"
+			rel="”nofollow”"
+			target="_blank">GitHub</a
+		>
 		verfügbar.
+	</p>
+
+	<p>
+		Die Inhalte werden über <a href="https://pages.cloudflare.com/" rel="”nofollow”" target="_blank"
+			>Cloudflare Pages</a
+		>
+		ausgeliefert, unter
+		<a href="https://sgb.hypotheses.org/" rel="”nofollow”" target="_blank">sgb.hypotheses.org</a>
+		verwaltet und von
+		<a href="https://hypotheses.org/" rel="”nofollow”" target="_blank">Hypotheses</a> langzeitarchiviert.
 	</p>
 </Container>
