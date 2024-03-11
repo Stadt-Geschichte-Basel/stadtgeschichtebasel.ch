71766c508a4443dd4f8fd0408e26c9911c0b7f9f
Moritz Mähr <maehr@users.noreply.github.com>
2024-03-11 11:14:08 +0100

    Barrierefreiheitserklärung (#115)
    * feat: fix #104
    
    * Fix: Update src/routes/+layout.svelte a11y
    
    Co-authored-by: Moritz Twente <127845092+mtwente@users.noreply.github.com>
    
    * fix: fix layout incl. #118
    
    ---------
    
    Co-authored-by: Moritz Twente <127845092+mtwente@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -59,11 +59,7 @@
 						aria-label="Open navigation menu"
 					>
 						<span>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								height="2em"
-								viewBox="0 0 448 512"
-								class="fill-white"
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-8 fill-white"
 								><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
 									d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
 								/></svg
@@ -72,12 +68,9 @@
 					</button>
 					<div class="h-12">
 						<a href="/" class="cursor-pointer" aria-label="Home">
-							<object
-								data="{assets}/favicon.svg"
-								class="h-full"
-								type="image/svg+xml"
-								aria-label=""
-							/>
+							<div
+								class="bg-[url('{assets}/favicon.svg')] h-full w-64 bg-contain bg-no-repeat"
+							></div>
 						</a>
 					</div>
 				</div></svelte:fragment
@@ -89,6 +82,7 @@
 							href="https://twitter.com/{config.twitterHandle}/"
 							aria-label="Twitter"
 							rel="noopener noreferrer"
+							target="_blank"
 						>
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-8 fill-white"
 								><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
@@ -102,6 +96,7 @@
 							href="https://www.instagram.com/{config.instagramHandle}/"
 							aria-label="Instagram"
 							rel="noopener noreferrer"
+							target="_blank"
 							><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-8 fill-white"
 								><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
 									d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
@@ -132,9 +127,11 @@
 			<section>
 				<p>
 					© 2024
-					<span class="mx-2 opacity-10">|</span>
+					<span class="mx-2 opacity-10" aria-hidden="true">|</span>
 					<a class="anchor" href="/datenschutzerklaerung">Datenschutz</a>
-					<span class="mx-2 opacity-10">|</span>
+					<span class="mx-2 opacity-10" aria-hidden="true">|</span>
+					<a class="anchor" href="/barrierefreiheitserklaerung">Barrierefreiheit</a>
+					<span class="mx-2 opacity-10" aria-hidden="true">|</span>
 					<a class="anchor" href="/credits">Credits</a>
 				</p>
 			</section>
