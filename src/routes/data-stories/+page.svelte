edd9b380417b3e269a3a3fad0863b2b1e9126f40
Moritz Mähr <maehr@users.noreply.github.com>
2024-03-12 16:29:23 +0100

    feat: fix #119

--- src/routes/data-stories/+page.svelte
+++ src/routes/data-stories/+page.svelte
@@ -2,18 +2,36 @@
 	import Container from '$lib/components/Container.svelte';
 	import Head from '$lib/components/Head.svelte';
 	import data from '$lib/data/datastories.json';
+
+	const whatIsADataStory =
+		'Data Storytelling ist eine innovative Methode, bei der erzählerische Elemente und Daten  miteinander kombiniert werden, um komplexe Informationen und Forschungsergebnisse in einer  zugänglichen und ansprechenden digitalen Form zu präsentieren. Im Kontext von  Stadt.Geschichte.Basel nutzen Autor*innen und Autoren-Kollektive diese Technik, um Geschichten  und Forschungsdaten aus dem Projekt aufzubereiten und so die faszinierende Historie von Basel  auf neue und spannende Weise zu vermitteln.';
+
+	function toLocaleDateString(date) {
+		return new Date(date).toLocaleDateString('de-CH');
+	}
 </script>
 
-<Head title="Data Stories | Hier finden Sie alle Data Stories" jsonLdDataType="Article" />
+<Head
+	title="Data Stories | Hier finden Sie alle Data Stories"
+	description={whatIsADataStory}
+	jsonLdDataType="Article"
+/>
 
 <Container>
 	<h1>Data Stories</h1>
-	<p>Hier finden Sie alle Data Stories.</p>
+	<p>Wir bieten Ihnen folgende Data Stories:</p>
 	<ul class="posts">
-		{#each data as { title, slug }}
+		{#each data as { title, slug, description, created }}
 			<li class="post">
-				<a href="/data-stories/{slug}" class="title">{title}</a>
+				<p>
+					<a href="/data-stories/{slug}" class="title">{title}</a>
+					{description} (erstellt am {toLocaleDateString(created)})
+				</p>
 			</li>
 		{/each}
-	</ul>
-</Container>
+		<h2>Was ist eine Datastory?</h2>
+		<p>
+			{whatIsADataStory}
+		</p>
+	</ul></Container
+>
