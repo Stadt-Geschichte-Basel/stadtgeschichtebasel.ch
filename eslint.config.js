978e9b07201741d7864be1608330633b1a2b8deb
Moritz Mähr <maehr@users.noreply.github.com>
2024-04-09 08:20:29 +0200

    ci: add ignores to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,19 @@ export default [
 	// js.configs.recommended,
 	...eslintPluginSvelte.configs['flat/recommended'],
 	{
+		ignores: [
+			'.DS_Store',
+			'node_modules',
+			'/build',
+			'/.svelte-kit',
+			'/package',
+			'.env',
+			'.env.*',
+			'!.env.example',
+			'pnpm-lock.yaml',
+			'package-lock.json',
+			'yarn.lock'
+		],
 		rules: {
 			// override/add rules settings here, such as:
 			// 'svelte/rule-name': 'error'
