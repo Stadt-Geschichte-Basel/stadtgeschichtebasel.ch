36350a2223acc1f4917a16015eff92b7a7aed4ef
Nico Görlich <73133626+koilebeit@users.noreply.github.com>
2024-08-16 14:14:32 +0200

    Linkedin (#184)
    * update dependencies
    
    * replace x with linkedin
    
    * update pnpm-lock

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -113,14 +113,14 @@
 				<ul class="grid grid-flow-col gap-4">
 					<li>
 						<a
-							href="https://twitter.com/{config.twitterHandle}/"
-							aria-label="Twitter"
+							href="https://www.linkedin.com/company/{config.linkedinHandle}/"
+							aria-label="LinkedIn"
 							rel="noopener noreferrer"
 							target="_blank"
 						>
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-8 fill-white"
-								><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-									d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
+								><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+									d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"
 								/></svg
 							></a
 						>
